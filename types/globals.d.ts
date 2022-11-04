18177b0ba7865c0fde925dd5ad7fa68a9d44e509
Pier Fumagalli <pier@usrz.com>
2022-11-04 19:53:23 +0100

    Inherit from PlugJS

--- types/globals.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference no-default-lib="true"/>
-
-/**
- * A pseudo-variable replaced by `ESBuild` resolving to either `__filename`
- * in CJS modules, or to `import.meta.url` in ESM modules.
- */
-declare const __fileurl: string
